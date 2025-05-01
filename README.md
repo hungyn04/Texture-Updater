207f53f827636b1ee69031a482267eb71fcb71c5
HungNguyen <www.thuphan@gmail.com>
2025-05-01 16:40:31 +0700

    Massive overhaul
    You can now move the view around, zoom in/out, use arrow key to select tile.
    
    This commit is a work-in-progress, right now it isn't working yet.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Texture-Updater
-Trash python script to easily add new textures to the old Minecraft PE (0.14.3) texture atlas
+
+A Python script to easily add new textures to the old Minecraft PE (0.14.3) texture atlas
 
 Install pygame before using the script.
 Support for TGA texture comming soon (current pygame TGA support is broken).
-Place the script, the "vanilla" folder and "data" folder into the same directory, or you can modify file paths in the script to your liking.
 This script is intended to get the job done in short amount of time, so expect the code not to be good and efficient.
--- texture_updater.py
+++ texture_updater.py
@@ -1,38 +1,21 @@
-import os, json
-import pygame as pg
+import os, json, math
 
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = ""
 os.chdir(os.path.realpath(os.path.dirname(__file__)))
-SOURCE = "./vanilla/textures/"
-TARGET = "./data/images/"
 
-
-with open(f"{TARGET}terrain.meta", "r") as inp:
-    terrain_dat = json.load(inp)
-
-w, h = terrain_dat[0]["uvs"][0][4:6]
-tw, th = terrain_dat[0]["uvs"][0][2:4]
-
-try:
-    with open("config.csv", "r") as inp:
-        rept = [i for i in inp.read().split(",")]
-except FileNotFoundError:
-    with open("config.csv", "x") as inp:
-        rept = []
-
-pg.init()
-scale_factor = 4
-screen = pg.display.set_mode((w * 2 + 50, h * 2), pg.RESIZABLE)
+import pygame as pg
 
 
 class render_control(object):
+    # class taken from my "flappy clone" project
     def __init__(self):
         pg.font.init()
-        self.font = pg.font.SysFont("Mojangles", 10)
-        self.bg = pg.surface.Surface((1, 1), pg.SRCALPHA).convert_alpha()
-        self.bg.fill((0, 0, 0, 120))
+        self.bg = pg.surface.Surface((1, 1), pg.SRCALPHA)
+        self.bg.fill((0, 0, 0, 150))
 
-    def text(self, content: str, des: pg.Surface, align: tuple = (0, 0), offset: tuple = (0, 0), area: tuple = None, with_bg: bool = False, color="white", antialiasing: bool = False):
+    def text(self, content: str, des: pg.Surface, align: tuple = (0, 0), offset: tuple = (0, 0), area: tuple = None, with_bg: bool = False, color="white", antialiasing: bool = False, size: float = 20):
         # align: (horizontal, vertical) ---- 0: left/top, 1: middle, 2: right/bottom
+        self.font = pg.font.SysFont("Mojangles", size)
         __line_list = content.split("\n")
         __line_count = len(__line_list)
         for __lindex, __lcurrent in enumerate(__line_list):
@@ -48,64 +31,129 @@ class render_control(object):
             des.blit(__text, __text_rect)
 
 
-pgc = pg.time.Clock()
 render = render_control()
-pg.display.set_caption("Preview output texture atlas")
 
-debug_surf = pg.Surface((w * scale_factor + 50, h * scale_factor), pg.SRCALPHA)
-scene_cp = mouse_shift = (0, 0)
+_conf = "config.conf"
+meta_path = ""
+sel_pos = 0
+scene_cp = [0, 0]
+scale = 2
+
+
+def rpath(p: str):
+    try:
+        path = os.path.relpath(os.path.realpath(p))
+        if path[0:2] == "..":
+            raise ValueError
+    except ValueError:
+        path = os.path.realpath(p)
+    return path
+
+
+if os.path.exists(rpath(_conf)):
+    print("Detected config.conf")
+else:
+    _conf = input("Path to config file (.conf, leave blank to create new one - drag file here for quick input): ")
+    if len(_conf.split(".")) == 1:
+        _conf = _conf.strip(".") + ".conf"
+try:
+    with open(_conf, "r", encoding="utf-8") as inp:
+        meta_path = rpath(inp.readline().strip())
+        if not os.path.exists(meta_path):
+            raise FileNotFoundError
+        tga_path = rpath(inp.readline().strip())
+        sel_pos = int(inp.readline().strip())
+        scene_cp[0], scene_cp[1], scale = list(map(float, inp.readline().strip().split(",")))
+        tex_repl = inp.readline().split(",")
+except FileNotFoundError:
+    print("Skipped .conf file, or file not found, or file contains error(s).")
+    while not os.path.exists(meta_path):
+        meta_path = rpath(input("Path to .meta file - drag file here for quick input: "))
+    tga_path = rpath(input("Path to target .tga file (leave blank to create output.tga - drag file here for quick input): "))
+    tex_repl = []
+
+pg.init()
+pg.display.set_caption("Edit texture atlas")
+screen = pg.display.set_mode((1280, 600), pg.RESIZABLE)
+pgc = pg.time.Clock()
+
+with open(meta_path, "r") as inp:
+    tex_pos = []
+    tex_name = []
+    for index, tile in enumerate(json.load(inp)):
+        tex_pos.extend([i + [index] for i in tile["uvs"]])
+        tex_name.append(tile["name"])
+    tex_count = len(tex_pos)
+    if len(tex_repl) != tex_count:
+        tex_repl = [""] * tex_count
+
+w, h = tex_pos[0][4:6]
+tw, th = tex_pos[0][2:4]
+tc = w // tw
 drag_active = False
+_exit = False
 
-_exit = _done = _selected = False
+upd_surf = pg.Surface((w, h), pg.SRCALPHA)
+try:
+    org_surf = pg.image.load(tga_path)
+except FileNotFoundError:
+    org_surf = pg.Surface((w, h), pg.SRCALPHA)
+
 while not _exit:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            _exit = True
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_ESCAPE:
-                exit()
-            if event.key == pg.K_TAB:
-                rept.append("0")
-            if event.key == pg.K_RETURN:
-                rept.append("1")
-            if event.key == pg.K_BACKSPACE:
-                rept.pop()
-            if event.key == pg.K_r:
-                with open("config.csv", "r") as inp:
-                    rept = [i for i in inp.read().split(",")]
-            _done = False
-        if event.type == pg.DROPFILE:
-            rept.append(event.file.split("\\")[-1].split(".")[0])
-            _done = False
-        if event.type == pg.MOUSEBUTTONDOWN:
-            mouse_shift = (event.pos[0] - scene_cp[0], event.pos[1] - scene_cp[1])
-            drag_active = True
-        if event.type == pg.MOUSEBUTTONUP:
-            drag_active = False
-        if event.type == pg.MOUSEMOTION and drag_active:
-            scene_cp = (event.pos[0] - mouse_shift[0], event.pos[1] - mouse_shift[1])
-    if not _done:
+    mx, my = pg.mouse.get_pos()
+    for ev in pg.event.get():
+        match ev.type:
+            case pg.QUIT:
+                _exit = True
+            case pg.KEYDOWN:
+                if ev.key == pg.K_ESCAPE:
+                    exit()
+                if ev.key == pg.K_RETURN:
+                    tex_repl[sel_pos] = "0"
+                if ev.key in (pg.K_DELETE, pg.K_BACKSPACE, pg.K_TAB):
+                    tex_repl[sel_pos] = ""
+                if ev.key in (pg.K_RIGHT, pg.K_TAB, pg.K_RETURN):
+                    sel_pos += 1
+                if ev.key in (pg.K_LEFT, pg.K_BACKSPACE):
+                    sel_pos -= 1
+                    sel_pos = 0 if (ev.key == pg.K_BACKSPACE and sel_pos < 1) else sel_pos
+                sel_pos = sel_pos + (ev.key == pg.K_DOWN) * tc - (ev.key == pg.K_UP) * tc
+            case pg.DROPFILE:
+                tex_repl[sel_pos] = os.path.split(ev.file)[-1]
+                sel_pos += 1 if sel_pos < tex_count else 0
+            case pg.MOUSEWHEEL:
+                _z = 1.25 if ev.y > 0 else 0.8
+                scene_cp = [round(mx + (scene_cp[0] - mx) * _z, 3), round(my + (scene_cp[1] - my) * _z, 3)]
+                scale = round(scale * _z, 3)
+            case pg.MOUSEBUTTONDOWN:
+                _ms = (mx - scene_cp[0], my - scene_cp[1])
+                drag_active = True
+            case pg.MOUSEBUTTONUP:
+                drag_active = False
+            case pg.MOUSEMOTION if drag_active:
+                scene_cp = [mx - _ms[0], my - _ms[1]]
+    sel_pos %= tex_count
+    """if not _done:
         ci = 0
         out_surf = pg.image.load(f"{TARGET}terrain-atlas.tga")
-        debug_surf.fill((0, 0, 0, 0))
-        for tile in terrain_dat:
+        for tile in tex_dat:
             for tp in tile["uvs"]:
-                if ci == len(rept):
+                if ci == len(tex_repl):
                     if _selected == True:
-                        debug_surf.blit(pg.transform.scale_by(pg.image.load(f"{SOURCE}blocks/structure_void.png"), scale_factor), (tp[0] * scale_factor, tp[1] * scale_factor))
+                        debug_surf.blit(pg.transform.scale_by(pg.image.load(f"{SOURCE}blocks/structure_void.png"), scale), (tp[0] * scale, tp[1] * scale))
                         _selected = False
-                    render.text(tile["name"], debug_surf, (0, 0), (tp[0] * scale_factor, tp[1] * scale_factor + (tp[0] // 16) % 3 * 12), (tw * scale_factor, th * scale_factor), with_bg=True)
-                    if tile == terrain_dat[-1]:
+                    render.text(tile["name"], debug_surf, (0, 0), (tp[0] * scale, tp[1] * scale + (tp[0] // 16) % 3 * 12), (tw * scale, th * scale), with_bg=True)
+                    if tile == tex_dat[-1]:
                         break
                     continue
-                if ci == len(rept) - 1:
+                if ci == len(tex_repl) - 1:
                     _selected = True
-                if rept[ci] == "0":
-                    debug_surf.fill((255, 0, 0, 150), (tp[0] * scale_factor, tp[1] * scale_factor, tw * scale_factor, th * scale_factor))
-                _rc = rept[ci]
-                if rept[ci] == "1":
+                if tex_repl[ci] == "0":
+                    debug_surf.fill((255, 0, 0, 150), (tp[0] * scale, tp[1] * scale, tw * scale, th * scale))
+                _rc = tex_repl[ci]
+                if tex_repl[ci] == "0":
                     _rc = tile["name"]
-                if (ci != len(rept)) and (rept[ci] != "0"):
+                if (ci != len(tex_repl)) and (tex_repl[ci] != "0"):
                     out_surf.fill((0, 0, 0, 0), (tp[0], tp[1], tw, th))
                     try:
                         out_surf.blit(pg.image.load(f"{SOURCE}blocks/{_rc}.png"), (tp[0], tp[1]))
@@ -113,21 +161,31 @@ while not _exit:
                         try:
                             out_surf.blit(pg.image.load(f"{SOURCE}blocks/{_rc}.tga"), (tp[0], tp[1]))
                         except FileNotFoundError:
-                            render.text("ERROR", debug_surf, (1, 1), (tp[0] * scale_factor, tp[1] * scale_factor), (tw * scale_factor, th * scale_factor), color="red")
-                ci = min(ci + 1, len(rept))
+                            render.text("ERROR", debug_surf, (1, 1), (tp[0] * scale, tp[1] * scale), (tw * scale, th * scale), color="red")
+                ci = min(ci + 1, len(tex_repl))
             else:
                 continue
             _done = True
-            break
+            break"""
     screen.fill((0, 0, 0))
-    screen.blits(((pg.transform.scale_by(out_surf, scale_factor), scene_cp), (debug_surf, scene_cp)))
-    render.text("Esc: Cancel changes\nTab: Skip tile\nEnter: Use current name\nBackspace: Remove last name\nR: Reload name\nDrag and drop file here to add to config.csv", screen, (0, 2), area=screen.get_size(), with_bg=True)
-    render.text(f"Dimension: {w}x{h}", screen, (2, 2), area=screen.get_size(), with_bg=True)
+    tex_sel_pos = tex_pos[sel_pos]
+    test_surf = pg.Surface((math.ceil(screen.get_width() / scale), math.ceil(screen.get_height() / scale)))
+    sx, sy = math.ceil(scene_cp[0] / scale) * scale, math.ceil(scene_cp[1] / scale) * scale
+
+    test_surf.blit(org_surf, (math.ceil(scene_cp[0] / scale), math.ceil(scene_cp[1] / scale)))
+    screen.blit(pg.transform.scale_by(test_surf, scale), (0, 0))
+    pg.draw.rect(screen, "white", (sx, sy, w * scale, h * scale), 1)
+    pg.draw.rect(screen, "red", (sx + math.ceil(tex_sel_pos[0] * scale), sy + math.ceil(tex_sel_pos[1] * scale), math.ceil(tw * scale), math.ceil(th * scale)), 2)
+    tname = f"{tex_name[tex_sel_pos[-1]]}{" -> " + tex_repl[sel_pos] if tex_repl[sel_pos] not in ["0", ""] else ""}"
+    render.text(tname, screen, offset=(sx + tex_sel_pos[0] * scale, sy + (tex_sel_pos[1] + th) * scale), area=(0, 0), with_bg=True)
+
+    render.text("Close program: Save changes and exit\nEsc: Cancel changes and exit\n\nTab: Skip name\nEnter: Use current name\nBackspace: Remove last name\nR: Reload original name from file\n\nMouse wheel: Zoom in/out\nMouse drag: Move around\nArrow key: Select tile\n\nDrag and drop file here to add its name to config.conf", screen, (0, 2), area=screen.get_size(), with_bg=True, size=10)
+    render.text(f"View position: x = {round(scene_cp[0])}, y = {round(scene_cp[1])}\nZoom level: {scale}x\n\nDimension: {w}x{h}\nTile size: {tw}x{th}", screen, (2, 2), area=screen.get_size(), with_bg=True, size=10)
     pg.display.update()
     pgc.tick(60)
 
-with open("config.csv", "w") as out:
-    out.write(",".join(rept))
+with open(f"{os.path.splitext(os.path.split(meta_path)[1])[0]}.conf", "w", encoding="utf-8") as out:
+    out.write("\n".join([meta_path, tga_path, str(sel_pos), ",".join(map(str, scene_cp + [scale])), ",".join(tex_repl)]))
 
-pg.image.save(out_surf, "test.png", "png")
+pg.image.save(org_surf, "test.png", "png")
 pg.quit()
