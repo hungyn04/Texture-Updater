e199da6b53d1fd0ba9be1323a145fab426f84a69
HungNguyen <www.thuphan@gmail.com>
2025-05-02 23:19:37 +0700

    Major changes
    Added many keyboard shortcut for convenient
    Added the ability to make the viewport follow the red selector
    Everything including zooming, view position, etc. will be saved to conf file
    Other bug fixes and improvements

--- texture_updater.py
+++ texture_updater.py
@@ -1,4 +1,4 @@
-import os, json, math
+import os, sys, json, math
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = ""
 os.chdir(os.path.realpath(os.path.dirname(__file__)))
@@ -12,9 +12,8 @@ class render_control(object):
         pg.font.init()
         self.font = (pg.font.Font("Mojangles.ttf", 10), pg.font.Font("Mojangles.ttf", 20), pg.font.Font("Mojangles.ttf", 30))
         self.bg = pg.surface.Surface((1, 1), pg.SRCALPHA)
-        self.bg.fill((0, 0, 0, 150))
 
-    def text(self, content: str, des: pg.Surface, align: tuple = (0, 0), offset: tuple = (0, 0), area: tuple = None, with_bg: bool = False, color="white", antialiasing: bool = False, size: int = 1):
+    def text(self, content: str, des: pg.Surface, align: tuple = (0, 0), offset: tuple = (0, 0), area: tuple = None, with_bg: int = 175, color="white", antialiasing: bool = False, size: int = 1):
         # align: (horizontal, vertical) ---- 0: left/top, 1: middle, 2: right/bottom
         __line_list = content.split("\n")
         __line_count = len(__line_list)
@@ -26,50 +25,89 @@ class render_control(object):
             __al_offset_y = (__text_rect.height * __lindex, __h / 2 - (__line_count * __text_rect.height) / 2 + __text_rect.height * __lindex, __h - __line_count * __text_rect.height + __text_rect.height * __lindex)
             __text_rect.x = __al_offset_x[align[0]] + offset[0]
             __text_rect.y = __al_offset_y[align[1]] + offset[1]
-            if with_bg:
+            if with_bg != 0:
+                self.bg.fill((0, 0, 0, with_bg))
                 des.blit(pg.transform.scale(self.bg, __text_rect.size), __text_rect)
             des.blit(__text, __text_rect)
+
+    def popup(self, content: str, duration: int, lasttime: int):
+        if pg.time.get_ticks() - lasttime < duration:
+            self.text(content, screen, (1, 1), area=screen.get_size(), with_bg=255, size=3)
+
+    def trigger_popup(self, content: str, duration: int = 3000):
+        global p_content, p_duration, p_ltime
+        p_content = content
+        p_duration = duration
+        p_ltime = pg.time.get_ticks()
 
 
 render = render_control()
 
-_conf = "config.conf"
+
+def rpath(inp_path: str, is_img: bool = False, allow_return_dir: bool = False):
+    if inp_path == "":
+        if allow_return_dir:
+            return (inp_path, False)
+        return inp_path
+    out = ""
+    is_dir = False
+    try:
+        out = os.path.relpath(os.path.realpath(inp_path))
+        if out[0:2] == "..":
+            raise ValueError
+    except ValueError:
+        out = os.path.realpath(inp_path)
+    if os.path.isfile(out):
+        if is_img:
+            try:
+                pg.image.load(out)
+            except:
+                out = ""
+    else:
+        is_dir = out != ""
+    if allow_return_dir:
+        out = (out, is_dir)
+    return out
+
+
+def save_conf():
+    _ = f"{os.path.splitext(os.path.split(meta_path)[1])[0]}.conf"
+    _dat = [meta_path, tga_path, home_dir, ",".join(map(str, (sel_pos, follow_selector))), ",".join(map(str, scene_cp + [scale])), ",".join(tex_repl)]
+    with open(_, "w", encoding="utf-8") as out:
+        out.write("\n".join(_dat))
+    return _
+
+
+def save_changes():
+    pg.image.save(org_surf, "test.png", "png")
+
+
 meta_path = ""
 sel_pos = 0
 scene_cp = [0, 0]
 scale = 2
+follow_selector = 0
+home_dir = "."
+_conf = sys.argv[-1] if len(sys.argv) > 1 else ""
 
-
-def rpath(p: str):
-    try:
-        path = os.path.relpath(os.path.realpath(p))
-        if path[0:2] == "..":
-            raise ValueError
-    except ValueError:
-        path = os.path.realpath(p)
-    return path
-
-
-if os.path.exists(rpath(_conf)):
-    print("Detected config.conf")
-else:
+if not os.path.exists(rpath(_conf)):
     _conf = input("Path to config file (.conf, leave blank to create new one - drag file here for quick input): ")
-    if len(_conf.split(".")) == 1:
-        _conf = _conf.strip(".") + ".conf"
+
 try:
     with open(_conf, "r", encoding="utf-8") as inp:
         meta_path = rpath(inp.readline().strip())
         if not os.path.exists(meta_path):
             raise FileNotFoundError
-        tga_path = rpath(inp.readline().strip())
-        sel_pos = int(inp.readline().strip())
-        scene_cp[0], scene_cp[1], scale = list(map(float, inp.readline().strip().split(",")))
+        tga_path = rpath(inp.readline().strip(), True)
+        home_dir = rpath(inp.readline().strip())
+        sel_pos, follow_selector = map(int, inp.readline().strip().split(","))
+        scene_cp[0], scene_cp[1], scale = map(float, inp.readline().strip().split(","))
         tex_repl = inp.readline().split(",")
-except FileNotFoundError:
+except:
     print("Skipped .conf file, or file not found, or file contains error(s).")
     while not os.path.exists(meta_path):
         meta_path = rpath(input("Path to .meta file - drag file here for quick input: "))
-    tga_path = rpath(input("Path to target .tga file (leave blank to create output.tga - drag file here for quick input): "))
+    tga_path = rpath(input("Path to target .tga file (leave blank to create output.tga - drag file here for quick input): "), True)
     tex_repl = []
 
 pg.init()
@@ -92,39 +130,88 @@ tw, th = tex_pos[0][2:4]
 tc = w // tw
 drag_active = False
 _exit = False
+_z = 1
+p_content = ""
+p_duration = 2500
+p_ltime = 0
 
+
+dbg_surf = pg.Surface((w, h), pg.SRCALPHA)
 upd_surf = pg.Surface((w, h), pg.SRCALPHA)
 try:
     org_surf = pg.image.load(tga_path)
 except FileNotFoundError:
     org_surf = pg.Surface((w, h), pg.SRCALPHA)
 
+
+def dbg_fill(is_valid: bool, spos: int = -1):
+    spos = sel_pos if spos == -1 else spos
+    color = (0, 0, 0) if is_valid else (0, 0, 0, 180)
+    dbg_surf.fill(color, tex_pos[spos][0:2] + [tw, th])
+
+
+def upd_fill(path: str = "", spos: int = -1):
+    spos = sel_pos if spos == -1 else spos
+    upd_surf.fill((0, 0, 0, 0), tex_pos[spos][0:2] + [tw, th])
+    if os.path.exists(rpath(path, True)):
+        upd_surf.blit(pg.transform.scale(pg.image.load(path), [tw, th]), tex_pos[spos][0:2])
+        return True
+    return False
+
+
+for id in range(tex_count):
+    dbg_fill(upd_fill(tex_repl[id], id), id)
+
 while not _exit:
     mx, my = pg.mouse.get_pos()
     for ev in pg.event.get():
         match ev.type:
             case pg.QUIT:
+                save_conf()
                 _exit = True
             case pg.KEYDOWN:
                 if ev.key == pg.K_ESCAPE:
                     exit()
+                if (ev.key == pg.K_s) and (pg.key.get_mods() & pg.KMOD_CTRL):
+                    render.trigger_popup(f'Changes saved to "{save_conf()}"')
+                    continue
                 if ev.key == pg.K_RETURN:
-                    tex_repl[sel_pos] = "0"
+                    _pos = range(tex_count) if (pg.key.get_mods() & pg.KMOD_CTRL) else range(sel_pos, sel_pos + 1)
+                    for i in _pos:
+                        _pth = os.path.join(home_dir, tex_name[tex_pos[i][-1]])
+                        _pth = _pth + (".png" if os.path.exists(_pth + ".png") else ".tga")
+                        tex_repl[i] = _pth
+                        dbg_fill(upd_fill(_pth, i), i)
+                    if i != sel_pos:
+                        render.trigger_popup(f"Successfully applied current name to {tex_count} tiles")
+                if ev.key in (pg.K_LSHIFT, pg.K_RSHIFT):
+                    follow_selector = 1 - follow_selector
                 if ev.key in (pg.K_DELETE, pg.K_BACKSPACE, pg.K_TAB):
-                    tex_repl[sel_pos] = ""
-                if ev.key in (pg.K_RIGHT, pg.K_TAB, pg.K_RETURN):
+                    _pos = range(tex_count) if (ev.key == pg.K_DELETE) and (pg.key.get_mods() & pg.KMOD_CTRL) else range(sel_pos, sel_pos + 1)
+                    for i in _pos:
+                        tex_repl[i] = ""
+                        dbg_fill(upd_fill(spos=i), spos=i)
+                    if i != sel_pos:
+                        render.trigger_popup(f"Successfully deleted name of {tex_count} tiles")
+                if ev.key in (pg.K_MINUS, pg.K_EQUALS):
+                    _z = 1.25 if ev.key == pg.K_EQUALS else 0.8
+                if ev.key in (pg.K_RIGHT, pg.K_d, pg.K_TAB, pg.K_RETURN):
                     sel_pos += 1
-                if ev.key in (pg.K_LEFT, pg.K_BACKSPACE):
+                if ev.key in (pg.K_LEFT, pg.K_a, pg.K_BACKSPACE):
                     sel_pos -= 1
                     sel_pos = 0 if (ev.key == pg.K_BACKSPACE and sel_pos < 1) else sel_pos
-                sel_pos = sel_pos + (ev.key == pg.K_DOWN) * tc - (ev.key == pg.K_UP) * tc
+                sel_pos = sel_pos + (ev.key in (pg.K_DOWN, pg.K_s)) * tc - (ev.key in (pg.K_UP, pg.K_w)) * tc
             case pg.DROPFILE:
-                tex_repl[sel_pos] = os.path.split(ev.file)[-1]
-                sel_pos += 1 if sel_pos < tex_count else 0
+                _chk = rpath(ev.file, True, True)
+                if _chk[1]:
+                    home_dir = _chk[0]
+                    render.trigger_popup(f'Current source directory changed to\n"{home_dir}"')
+                else:
+                    tex_repl[sel_pos] = _chk[0]
+                    dbg_fill(upd_fill(tex_repl[sel_pos]))
+                    sel_pos += 1 if sel_pos < tex_count else 0
             case pg.MOUSEWHEEL:
                 _z = 1.25 if ev.y > 0 else 0.8
-                scene_cp = [round(mx + (scene_cp[0] - mx) * _z, 3), round(my + (scene_cp[1] - my) * _z, 3)]
-                scale = round(scale * _z, 3)
             case pg.MOUSEBUTTONDOWN:
                 _ms = (mx - scene_cp[0], my - scene_cp[1])
                 drag_active = True
@@ -132,60 +219,32 @@ while not _exit:
                 drag_active = False
             case pg.MOUSEMOTION if drag_active:
                 scene_cp = [mx - _ms[0], my - _ms[1]]
+                follow_selector = 0
+    scene_cp = [round(mx + (scene_cp[0] - mx) * _z, 3), round(my + (scene_cp[1] - my) * _z, 3)]
+    scale = round(scale * _z, 3)
+    _z = 1
     sel_pos %= tex_count
-    """if not _done:
-        ci = 0
-        out_surf = pg.image.load(f"{TARGET}terrain-atlas.tga")
-        for tile in tex_dat:
-            for tp in tile["uvs"]:
-                if ci == len(tex_repl):
-                    if _selected == True:
-                        debug_surf.blit(pg.transform.scale_by(pg.image.load(f"{SOURCE}blocks/structure_void.png"), scale), (tp[0] * scale, tp[1] * scale))
-                        _selected = False
-                    render.text(tile["name"], debug_surf, (0, 0), (tp[0] * scale, tp[1] * scale + (tp[0] // 16) % 3 * 12), (tw * scale, th * scale), with_bg=True)
-                    if tile == tex_dat[-1]:
-                        break
-                    continue
-                if ci == len(tex_repl) - 1:
-                    _selected = True
-                if tex_repl[ci] == "0":
-                    debug_surf.fill((255, 0, 0, 150), (tp[0] * scale, tp[1] * scale, tw * scale, th * scale))
-                _rc = tex_repl[ci]
-                if tex_repl[ci] == "0":
-                    _rc = tile["name"]
-                if (ci != len(tex_repl)) and (tex_repl[ci] != "0"):
-                    out_surf.fill((0, 0, 0, 0), (tp[0], tp[1], tw, th))
-                    try:
-                        out_surf.blit(pg.image.load(f"{SOURCE}blocks/{_rc}.png"), (tp[0], tp[1]))
-                    except FileNotFoundError:
-                        try:
-                            out_surf.blit(pg.image.load(f"{SOURCE}blocks/{_rc}.tga"), (tp[0], tp[1]))
-                        except FileNotFoundError:
-                            render.text("ERROR", debug_surf, (1, 1), (tp[0] * scale, tp[1] * scale), (tw * scale, th * scale), color="red")
-                ci = min(ci + 1, len(tex_repl))
-            else:
-                continue
-            _done = True
-            break"""
     screen.fill((0, 0, 0))
     tex_sel_pos = tex_pos[sel_pos]
     test_surf = pg.Surface((math.ceil(screen.get_width() / scale), math.ceil(screen.get_height() / scale)))
+    if follow_selector:
+        scene_cp = [screen.get_width() / 2 - tw * scale / 2 - tex_sel_pos[0] * scale, screen.get_height() / 2 - th * scale / 2 - tex_sel_pos[1] * scale]
     sx, sy = math.ceil(scene_cp[0] / scale) * scale, math.ceil(scene_cp[1] / scale) * scale
-
     test_surf.blit(org_surf, (math.ceil(scene_cp[0] / scale), math.ceil(scene_cp[1] / scale)))
+    test_surf.blit(dbg_surf, (math.ceil(scene_cp[0] / scale), math.ceil(scene_cp[1] / scale)))
+    test_surf.blit(upd_surf, (math.ceil(scene_cp[0] / scale), math.ceil(scene_cp[1] / scale)))
     screen.blit(pg.transform.scale_by(test_surf, scale), (0, 0))
+
     pg.draw.rect(screen, "white", (sx, sy, w * scale, h * scale), 1)
     pg.draw.rect(screen, "red", (sx + math.ceil(tex_sel_pos[0] * scale), sy + math.ceil(tex_sel_pos[1] * scale), math.ceil(tw * scale), math.ceil(th * scale)), 2)
     tname = f"{tex_name[tex_sel_pos[-1]]}{" -> " + tex_repl[sel_pos] if tex_repl[sel_pos] not in ["0", ""] else ""}"
-    render.text(tname, screen, offset=(sx + tex_sel_pos[0] * scale, sy + (tex_sel_pos[1] + th) * scale), area=(0, 0), with_bg=True, size=2)
+    render.text(tname, screen, offset=(sx + tex_sel_pos[0] * scale, sy + (tex_sel_pos[1] + th) * scale), area=(0, 0), size=2)
+    screen.blit(pg.transform.scale_by(org_surf.subsurface((tex_sel_pos[0:2] + [tw, th])), max(3, scale)), (sx + tex_sel_pos[0] * scale, sy + (tex_sel_pos[1] + th) * scale + 23))
 
-    render.text("Close program: Save changes and exit\nEsc: Cancel changes and exit\n\nTab: Skip name\nEnter: Use current name\nBackspace: Remove last name\nR: Reload original name from file\n\nMouse wheel: Zoom in/out\nMouse drag: Move around\nArrow key: Select tile\n\nDrag and drop file here to add its name to config.conf", screen, (0, 2), area=screen.get_size(), with_bg=True, size=1)
-    render.text(f"View position: x = {round(scene_cp[0])}, y = {round(scene_cp[1])}\nZoom level: {scale}x\n\nDimension: {w}x{h}\nTile size: {tw}x{th}", screen, (2, 2), area=screen.get_size(), with_bg=True, size=1)
+    render.text("Close program: Save changes and exit\nEsc: Cancel changes and exit\nCtrl + S: Save changes\n\nDelete: Remove current name\n(Ctrl + Delete: Apply to all)\nTab: Remove current name and move forward\nBackspace: Remove current name and move backward\nEnter: Use current name from home directory\n(Ctrl + Enter: Apply to all)\n\nMouse wheel, +, -: Zoom in/out\nMouse drag: Move around\nArrow key, WASD: Select tile\nShift: Toggle follow selector mode\n\nDrag and drop file here for quick replace\nDrag and drop folder here to change home directory", screen, (0, 2), area=screen.get_size(), size=1)
+    render.text(f"{"Follow selector mode" if follow_selector else ""}\n\nView position: x = {round(scene_cp[0])}, y = {round(scene_cp[1])}\nZoom level: {scale}x\n\nDimension: {w}x{h}\nTile size: {tw}x{th}\n\nHome directory: {home_dir}", screen, (2, 2), area=screen.get_size(), size=1)
+    render.popup(p_content, p_duration, p_ltime)
     pg.display.update()
     pgc.tick(60)
 
-with open(f"{os.path.splitext(os.path.split(meta_path)[1])[0]}.conf", "w", encoding="utf-8") as out:
-    out.write("\n".join([meta_path, tga_path, str(sel_pos), ",".join(map(str, scene_cp + [scale])), ",".join(tex_repl)]))
-
-pg.image.save(org_surf, "test.png", "png")
 pg.quit()
