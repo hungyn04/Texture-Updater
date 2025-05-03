d871dbb35337a56eaee64b01488500a69db0f6b3
HungNguyen <www.thuphan@gmail.com>
2025-05-03 21:19:27 +0700

    Finalized
    Added TGA support via PIL library
    Removed Mojangles font
    Added new shortcut
    Added original texture preview
    Saving to the original file will now automatically create a backup
    Other changes and bug fixes

--- texture_updater.py
+++ texture_updater.py
@@ -1,8 +1,9 @@
-import os, sys, json, math
+import os, sys, json, math, PIL
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = ""
 os.chdir(os.path.realpath(os.path.dirname(__file__)))
 
+import PIL.Image
 import pygame as pg
 
 
@@ -10,7 +11,7 @@ class render_control(object):
     # class taken from my "flappy clone" project
     def __init__(self):
         pg.font.init()
-        self.font = (pg.font.Font("Mojangles.ttf", 10), pg.font.Font("Mojangles.ttf", 20), pg.font.Font("Mojangles.ttf", 30))
+        self.font = [pg.font.SysFont("monospace", 12), pg.font.SysFont("monospace", 20), pg.font.SysFont("monospace", 30)]
         self.bg = pg.surface.Surface((1, 1), pg.SRCALPHA)
 
     def text(self, content: str, des: pg.Surface, align: tuple = (0, 0), offset: tuple = (0, 0), area: tuple = None, with_bg: int = 175, color="white", antialiasing: bool = False, size: int = 1):
@@ -71,24 +72,39 @@ def rpath(inp_path: str, is_img: bool = False, allow_return_dir: bool = False):
 
 
 def save_conf():
-    _ = f"{os.path.splitext(os.path.split(meta_path)[1])[0]}.conf"
-    _dat = [meta_path, tga_path, home_dir, ",".join(map(str, (sel_pos, follow_selector))), ",".join(map(str, scene_cp + [scale])), ",".join(tex_repl)]
-    with open(_, "w", encoding="utf-8") as out:
+    _n = f"{os.path.splitext(os.path.split(meta_path)[1])[0]}.conf"
+    _dat = [meta_path, tga_path, tga_path_original, source_dir, ",".join(map(str, (sel_pos, follow_selector))), ",".join(map(str, scene_cp + [scale])), ",".join(tex_repl)]
+    with open(_n, "w", encoding="utf-8") as out:
         out.write("\n".join(_dat))
-    return _
+    return _n
 
 
-def save_changes():
-    pg.image.save(org_surf, "test.png", "png")
+def export_tex():
+    global tga_path_nobackup, tga_path_original
+    _tgap = os.path.split(tga_path)
+    _n = os.path.splitext(_tgap[1])
+    _f = f"{_n[0]}{_n[1]}"
+    _surf = org_surf.copy()
+    _surf.blit(upd_mask_surf, (0, 0), special_flags=pg.BLEND_RGBA_MULT)
+    _surf.blit(upd_surf, (0, 0))
+    _new_path = os.path.join(_tgap[0], _n[0] + "_original" + _n[1])
+    if not os.path.exists(_new_path) and not tga_path_nobackup:
+        os.rename(tga_path, _new_path)
+        tga_path_original = _new_path
+        tga_path_nobackup = True
+    PIL.Image.frombytes("RGBA", (w, h), pg.image.tobytes(_surf, "RGBA")).save(tga_path)
+    return _f
 
 
-meta_path = ""
 sel_pos = 0
 scene_cp = [0, 0]
 scale = 2
 follow_selector = 0
-home_dir = "."
+source_dir = "."
 _conf = sys.argv[-1] if len(sys.argv) > 1 else ""
+meta_path = ""
+tga_path_nobackup = False
+tga_path_original = ""
 
 if not os.path.exists(rpath(_conf)):
     _conf = input("Path to config file (.conf, leave blank to create new one - drag file here for quick input): ")
@@ -99,7 +115,9 @@ try:
         if not os.path.exists(meta_path):
             raise FileNotFoundError
         tga_path = rpath(inp.readline().strip(), True)
-        home_dir = rpath(inp.readline().strip())
+        tga_path_original = rpath(inp.readline().strip(), True)
+        tga_path_nobackup = True
+        source_dir = rpath(inp.readline().strip())
         sel_pos, follow_selector = map(int, inp.readline().strip().split(","))
         scene_cp[0], scene_cp[1], scale = map(float, inp.readline().strip().split(","))
         tex_repl = inp.readline().split(",")
@@ -137,26 +155,40 @@ p_ltime = 0
 
 
 dbg_surf = pg.Surface((w, h), pg.SRCALPHA)
-upd_surf = pg.Surface((w, h), pg.SRCALPHA)
+upd_surf = dbg_surf.copy()
+upd_mask_surf = dbg_surf.copy()
+upd_mask_surf.fill("white", (0, 0, w, h))
 try:
-    org_surf = pg.image.load(tga_path)
+    org_surf = pg.image.load(tga_path_original)
+    tga_path_nobackup = True
 except FileNotFoundError:
-    org_surf = pg.Surface((w, h), pg.SRCALPHA)
+    try:
+        org_surf = pg.image.load(tga_path)
+    except FileNotFoundError:
+        tga_path = "output.png"
+        tga_path_nobackup = True
+        org_surf = dbg_surf.copy()
 
 
-def dbg_fill(is_valid: bool, spos: int = -1):
+def dbg_fill(state: int, spos: int = -1):
+    # state: 0 - no texture, 1 - normal, -1 - texture error
     spos = sel_pos if spos == -1 else spos
-    color = (0, 0, 0) if is_valid else (0, 0, 0, 180)
+    color = (0, 0, 0) if state == 1 else (0, 0, 0, 180)
     dbg_surf.fill(color, tex_pos[spos][0:2] + [tw, th])
+    if state == -1:
+        render.text("?", dbg_surf, (1, 1), (tex_pos[spos][0:2]), area=(tw, th), with_bg=0, color="red")
 
 
 def upd_fill(path: str = "", spos: int = -1):
     spos = sel_pos if spos == -1 else spos
-    upd_surf.fill((0, 0, 0, 0), tex_pos[spos][0:2] + [tw, th])
+    _cr = tex_pos[spos][0:2] + [tw, th]
+    upd_surf.fill((0, 0, 0, 0), _cr)
+    upd_mask_surf.fill("white", _cr)
     if os.path.exists(rpath(path, True)):
         upd_surf.blit(pg.transform.scale(pg.image.load(path), [tw, th]), tex_pos[spos][0:2])
-        return True
-    return False
+        upd_mask_surf.fill((0, 0, 0, 0), _cr)
+        return 1
+    return 0 if path == "" else -1
 
 
 for id in range(tex_count):
@@ -175,11 +207,14 @@ while not _exit:
                 if (ev.key == pg.K_s) and (pg.key.get_mods() & pg.KMOD_CTRL):
                     render.trigger_popup(f'Changes saved to "{save_conf()}"')
                     continue
+                if (ev.key == pg.K_e) and (pg.key.get_mods() & pg.KMOD_CTRL):
+                    render.trigger_popup(f'Changes saved to "{export_tex()}"')
+                    continue
                 if ev.key == pg.K_RETURN:
                     _pos = range(tex_count) if (pg.key.get_mods() & pg.KMOD_CTRL) else range(sel_pos, sel_pos + 1)
                     for i in _pos:
-                        _pth = os.path.join(home_dir, tex_name[tex_pos[i][-1]])
-                        _pth = _pth + (".png" if os.path.exists(_pth + ".png") else ".tga")
+                        _pth = os.path.join(source_dir, tex_name[tex_pos[i][-1]])
+                        _pth = _pth + (".tga" if os.path.exists(_pth + ".tga") else ".png")
                         tex_repl[i] = _pth
                         dbg_fill(upd_fill(_pth, i), i)
                     if i != sel_pos:
@@ -204,8 +239,8 @@ while not _exit:
             case pg.DROPFILE:
                 _chk = rpath(ev.file, True, True)
                 if _chk[1]:
-                    home_dir = _chk[0]
-                    render.trigger_popup(f'Current source directory changed to\n"{home_dir}"')
+                    source_dir = _chk[0]
+                    render.trigger_popup(f'Current source directory changed to\n"{source_dir}"')
                 else:
                     tex_repl[sel_pos] = _chk[0]
                     dbg_fill(upd_fill(tex_repl[sel_pos]))
@@ -241,8 +276,8 @@ while not _exit:
     render.text(tname, screen, offset=(sx + tex_sel_pos[0] * scale, sy + (tex_sel_pos[1] + th) * scale), area=(0, 0), size=2)
     screen.blit(pg.transform.scale_by(org_surf.subsurface((tex_sel_pos[0:2] + [tw, th])), max(3, scale)), (sx + tex_sel_pos[0] * scale, sy + (tex_sel_pos[1] + th) * scale + 23))
 
-    render.text("Close program: Save changes and exit\nEsc: Cancel changes and exit\nCtrl + S: Save changes\n\nDelete: Remove current name\n(Ctrl + Delete: Apply to all)\nTab: Remove current name and move forward\nBackspace: Remove current name and move backward\nEnter: Use current name from home directory\n(Ctrl + Enter: Apply to all)\n\nMouse wheel, +, -: Zoom in/out\nMouse drag: Move around\nArrow key, WASD: Select tile\nShift: Toggle follow selector mode\n\nDrag and drop file here for quick replace\nDrag and drop folder here to change home directory", screen, (0, 2), area=screen.get_size(), size=1)
-    render.text(f"{"Follow selector mode" if follow_selector else ""}\n\nView position: x = {round(scene_cp[0])}, y = {round(scene_cp[1])}\nZoom level: {scale}x\n\nDimension: {w}x{h}\nTile size: {tw}x{th}\n\nHome directory: {home_dir}", screen, (2, 2), area=screen.get_size(), size=1)
+    render.text("Close program: Save changes and exit\nEsc: Cancel changes and exit\nCtrl + S: Save changes\nCtrl + E: Export changes to image\n\nDel: Remove current name\n(Ctrl + Del: Apply to all)\nTab: Remove current name and move forward\nBackspace: Remove current name and move backward\nEnter: Use current name from source directory\n(Ctrl + Enter: Apply to all)\n\nMouse wheel, +, -: Zoom in/out\nMouse drag: Move around\nArrow key, WASD: Select tile\nShift: Toggle follow selector mode\n\nDrag and drop file here for quick replace\nDrag and drop folder here to change source directory", screen, (0, 2), area=screen.get_size(), size=1)
+    render.text(f"{"Follow selector mode" if follow_selector else ""}\n\nView position: x = {round(scene_cp[0])}, y = {round(scene_cp[1])}\nZoom level: {scale}x\n\nDimension: {w}x{h}\nTile size: {tw}x{th}\n\nSource directory: {source_dir}", screen, (2, 2), area=screen.get_size(), size=1)
     render.popup(p_content, p_duration, p_ltime)
     pg.display.update()
     pgc.tick(60)
